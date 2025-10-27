3893f2c8ea458ed7923a0d7a06e08ed762f35d6d
NataliaPriore <priore.natalia@alu.ing.unlp.edu.ar>
2025-10-27 13:16:30 -0300

    A

--- src/DAO/UsuarioDAOimple.java
+++ src/DAO/UsuarioDAOimple.java
@@ -6,7 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
 import Usuario.Usuario;
 import Usuario.Administrador;
 import Usuario.Cliente;
